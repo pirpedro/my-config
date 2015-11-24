956d7edeb0f4f3c5b361d99cb27d1c27b114bd9a
Pedro <pir.pedro@gmail.com>
2015-11-24 16:49:19 -0200

    Create conf file for my-config script

--- packages/02-git.sh
+++ packages/02-git.sh
@@ -5,7 +5,8 @@ case "$1" in
 install)
 	git config --global user.email "pir.pedro@gmail.com"
 	git config --global user.name "Pedro"
-	git config --global core.excludesfile ~/.gitignore_global
+             git config --global push.default simple
+	git config --global core.excludesfile ~/.git/gitignore
 
 ;;
 remove)
