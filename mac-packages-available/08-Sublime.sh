e7477666a5607b15f366a52fadb1ee8bba313835
Pedro <pir.pedro@gmail.com>
2016-02-17 16:39:57 -0200

    Include some packages in ubuntu version

--- mac-packages-available/08-Sublime.sh
+++ mac-packages-available/08-Sublime.sh
@@ -5,6 +5,11 @@ case "$1" in
 install)
 	brew cask install sublime-text3
     ln -s ~/Applications/Sublime\ Text.app /Applications/
+    #install sublime package manager
+    wget -P ~/Library/Application Support/Sublime Text 2/Installed\ Packages/ https://packagecontrol.io/Package%20Control.sublime-package
+
+    #TODO link dropbox installed packages
+
 ;;
 remove)
 ;;
