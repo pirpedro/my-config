e6f71a22cf728d9c842fade0c02ddd453c25bb9d
Pedro <pir.pedro@gmail.com>
2016-03-14 14:38:43 -0300

    Include moodle script

--- ubuntu-packages-available/14-wordpress.sh
+++ ubuntu-packages-available/14-wordpress.sh
@@ -5,6 +5,11 @@ case "$1" in
 install)
     sudo apt-get install php5-gd
     wget http://wordpress.org/latest.tar.gz
+    tar -xzvf latest.tar.gz
+    mysql -u root -p
+
+
+    #TODO https://www.digitalocean.com/community/tutorials/how-to-install-wordpress-on-ubuntu-12-04
 
 ;;
 remove)
