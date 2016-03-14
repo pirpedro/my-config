ca4cd21b073217616d31265d58cc1c4304f1ce39
Pedro <pir.pedro@gmail.com>
2016-03-14 16:45:38 -0300

    [Ubuntu] Include chrome installation

--- ubuntu-packages-available/08-Sublime.sh
+++ ubuntu-packages-available/08-Sublime.sh
@@ -3,9 +3,7 @@ source $3/functions.sh
 
 case "$1" in
 install)
-	brew cask install sublime-text3
-    ln -s ~/Applications/Sublime\ Text.app /Applications/
-    #install sublime package manager
+
     wget -P ~/.config/sublime-text-3/Installed\ Packages/ https://packagecontrol.io/Package%20Control.sublime-package
 
     #TODO link dropbox installed packages
