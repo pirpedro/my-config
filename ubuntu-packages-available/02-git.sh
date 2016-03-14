4680faec53a73a538e024022972943bbe9ae5668
Pedro <pir.pedro@gmail.com>
2016-03-14 16:53:39 -0300

    [Ubuntu] Change Sublime installation

--- ubuntu-packages-available/02-git.sh
+++ ubuntu-packages-available/02-git.sh
@@ -3,6 +3,7 @@ source $3/functions.sh
 
 case "$1" in
 install)
+    sudo apt-get install git git-core
 ;;
 remove)
 ;;
--- ubuntu-packages-available/08-Sublime.sh
+++ ubuntu-packages-available/08-Sublime.sh
@@ -3,7 +3,9 @@ source $3/functions.sh
 
 case "$1" in
 install)
-
+    sudo add-apt-repository ppa:webupd8team/sublime-text-3
+    sudo apt-get update
+    sudo apt-get install sublime-text-installer
     wget -P ~/.config/sublime-text-3/Installed\ Packages/ https://packagecontrol.io/Package%20Control.sublime-package
 
     #TODO link dropbox installed packages
