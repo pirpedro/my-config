159138c3dde83679a4989f9469f17712096d7ced
Pedro <pir.pedro@gmail.com>
2016-07-11 15:57:07 -0300

    Change ruby configuration (mac/linux)

---
+++ ubuntu-packages-available/19-ruby.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+source $3/functions.sh
+
+case "$1" in
+install)
+  sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev
+  git clone git://github.com/sstephenson/rbenv.git ~/.rbenv
+  __my_env "PATH" "\$HOME/.rbenv/bin:\$PATH"
+  __my_env "if which rbenv > /dev/null; then eval \"\$(rbenv init -)\"; fi"
+
+  git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+  __my_env "PATH" "\$HOME/.rbenv/plugins/ruby-build/bin:\$PATH"
+
+  rbenv install 2.3.0
+  rbenv global 2.3.0
+  echo "gem: --no-document" > ~/.gemrc
+  gem install bundler
+  gem install rails 4.2.5.1
+  gem install pry
+  rbenv rehash
+
+;;
+remove)
+;;
+esac
