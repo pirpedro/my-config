b6e4b852cf1119288f241c1882fe4d732923d53c
Pedro <pir.pedro@gmail.com>
2016-08-29 13:51:06 -0300

    Include java (linux)

---
+++ ubuntu-packages-available/20-java.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+source $3/functions.sh
+
+case "$1" in
+install)
+
+    sudo apt-get remove openjdk*
+
+    #adding oracle java repository
+    sudo add-apt-repository ppa:webupd8team/java
+    sudo apt-get update
+    #installing jre 1.8
+    sudo apt-get install oracle-java8-installer
+
+;;
+remove)
+;;
+esac
