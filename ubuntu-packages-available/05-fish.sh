ca38d0974b89ed58acce7c0b2668dbc3ec7bd4f9
Pedro <pir.pedro@gmail.com>
2016-09-02 15:32:38 -0300

    Include actions disable and enable for my-config bin

--- ubuntu-packages-available/05-fish.sh
+++ ubuntu-packages-available/05-fish.sh
@@ -3,21 +3,11 @@ source $3/functions.sh
 
 case "$1" in
 install)
-	if [ $(isMac) == "1" ]; then 
-		if [ $(exists brew) ]; then
-			brew install fish
-			echo "/usr/local/bin/fish" | sudo tee -a /etc/shells
-			sudo -u $SUDO_USER chsh -s /usr/local/bin/fish
-		else
-			echo "Need HomeBrew Installation"
-		fi	
-	     		
-	else
-		apt-add-repository ppa:fish-shell/release-2
-                apt-get update
-		apt-get install fish
-		sudo -u $SUDO_USER chsh -s /usr/bin/fish
-	fi
+	apt-add-repository ppa:fish-shell/release-2
+  apt-get update
+	apt-get install fish
+	sudo -u $SUDO_USER chsh -s /usr/bin/fish
+
 ;;
 remove)
 ;;
