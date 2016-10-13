b4638f3a03680ec34b2735830a6f38b658bc8e5e
Pedro Rodrigues <pir.pedro@gmail.com>
2016-10-13 08:29:47 -0300

    Vim package (Ubuntu)

--- ubuntu-packages-available/04-vim.sh
+++ ubuntu-packages-available/04-vim.sh
@@ -3,6 +3,9 @@ source $3/functions.sh
 
 case "$1" in
 install)
+  sudo apt-get install -y vim
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+  vim +PluginInstall +qall
 ;;
 remove)
 ;;
