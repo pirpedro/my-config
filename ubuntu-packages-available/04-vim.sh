1fef61385b7e5384d69f2420a63e9fc77d3f3301
Pedro Rodrigues <pir.pedro@gmail.com>
2016-10-14 14:35:22 -0300

    Minor changes

--- ubuntu-packages-available/04-vim.sh
+++ ubuntu-packages-available/04-vim.sh
@@ -5,8 +5,14 @@ case "$1" in
 install)
   sudo apt-get install -y vim
   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-  vim +PluginInstall +qall
   __my_sync ~/.vimrc
+
+  #install exuberant-ctags
+  sudo apt-get install -y exuberant-ctags 
+
+
+  vim +PluginInstall +qall
+
 ;;
 remove)
 ;;
