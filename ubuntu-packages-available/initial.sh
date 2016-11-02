9d9a74894d0ecd14aeb8d93bebfe51aab6d5b0ab
Pedro Rodrigues <pir.pedro@gmail.com>
2016-11-02 20:14:23 -0200

    Include 'init' command.
    Init command prepare machine to further commands usage.

--- ubuntu-packages-available/initial.sh
+++ ubuntu-packages-available/initial.sh
@@ -3,19 +3,19 @@ source $MY_CONFIG_EXT/functions.sh
 
 case "$1" in
 install)
-    if [ ! -e ~/.bashrc ]; then
+    if [ ! -f ~/.bashrc ]; then
         touch ~/.bashrc
     fi
 
-    if [ ! -e ~/.bash_profile ]; then
+    if [ ! -f ~/.bash_profile ]; then
         touch ~/.bash_profile
     fi
 
-    [ -d ~/.bash] || mkdir ~/.bash
-    touch ~/.bash/my-config.sh
+    test -d ~/.bash || mkdir ~/.bash
+    test -f ~/.bash/my-config.sh || touch ~/.bash/my-config.sh
+    test -f ~/.bash/.bash_aliases || touch ~/.bash/.bash_aliases
     chmod +x ~/.bash/my-config.sh
-    touch ~/.bash/.bash_aliases
-  	chmod +x ~/.bash/.bash_aliases
+    chmod +x ~/.bash/.bash_aliases
 
     echo "###############   my-config configuration script   ###############
 if [ -d ~/.bash ]; then
@@ -40,13 +40,13 @@ fi
 ##################################################################" >> ~/.bashrc
 
 #Ubuntu themes
-sudo apt-get install -y unity-tweak-tool
-sudo apt-add-repository ppa:tista/adapta -y
-sudo apt-get install adapta-gtk-theme
+#sudo apt-get install -y unity-tweak-tool
+#sudo apt-add-repository ppa:tista/adapta -y
+#sudo apt-get install adapta-gtk-theme
 
-sudo apt-add-repository ppa:numix/ppa -y
-sudo apt-get update
-sudo apt-get install numix-icon-theme-circle
+#sudo apt-add-repository ppa:numix/ppa -y
+#sudo apt-get update
+#sudo apt-get install numix-icon-theme-circle
 
 ;;
 remove)
