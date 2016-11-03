7a5b8630ceeb58d02be63261362b22baa8e415e8
Pedro Rodrigues <pir.pedro@gmail.com>
2016-11-03 21:04:07 -0200

    [brew OSX] Include gnu-sed installation.

--- mac-packages-available/homebrew.sh
+++ mac-packages-available/homebrew.sh
@@ -25,31 +25,15 @@ install)
         __brew_tap caskroom/fonts
         __brew_tap caskroom/eid
         __brew_install homebrew/completions/brew-cask-completion
+
+    __brew_install gnu-sed --with-default-names
+
 	else
 		echo "Ruby not installed";
 	fi
 
 ;;
 remove)
-#	set -e
-
-#	/usr/bin/which -s git || abort "brew install git first!"
-#	test -d /usr/local/.git || abort "brew update first!"
-
-#	cd `brew --prefix`
-#	git checkout master
-#	git ls-files -z | pbcopy
-#	rm -rf Cellar
-#	bin/brew prune
-#	pbpaste | xargs -0 rm
-#	rm -r Library/Homebrew Library/Aliases Library/Formula Library/Contributions
-#	test -d Library/LinkedKegs && rm -r Library/LinkedKegs
-#	rmdir -p bin Library share/man/man1 2> /dev/null
-#	rm -rf .git
-#	rm -rf ~/Library/Caches/Homebrew
-#	rm -rf ~/Library/Logs/Homebrew
-#	rm -rf /Library/Caches/Homebrew
-
    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall)"
    sudo chmod 0755 /usr/local
    sudo chgrp wheel /usr/local
