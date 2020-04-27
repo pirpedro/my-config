36715daccb85e51dc7c30fb80ac15fce76ee5f90
Pedro Rodrigues <pir.pedro@gmail.com>
2020-04-26 23:11:58 -0300

    Change editor to vscode.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 case "$1" in
-  open) ${SHELL_IDE:-atom} . ~/.myconfig              ;;
+  open) ${SHELL_IDE:-code} . ~/.myconfig              ;;
   ssh) vagrant up; vagrant ssh                        ;;
   test) test/run_core_tests.sh                        ;;
   test_all) test/run_all_tests.sh                     ;;
